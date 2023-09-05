bb7222e27435925d432aa412d34e1f3000338935
Yared <yared241993@gmail.com>
2023-09-05 12:04:49 +0300

    updated the form items center

--- src/components/forms/RegisterForm.jsx
+++ src/components/forms/RegisterForm.jsx
@@ -6,13 +6,16 @@ function RegisterForm() {
     const { register, handleSubmit,  formState: { errors } } = useForm();
     const onSubmit = data => console.log(data);
   return (
-    <div>
+    <div className='text-center items-center justify-center '>
       <h1>Register</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
    
-      <input {...register("titel")} className='p-2 border' />
+      <input {...register("titel")} className='p-2 border ' />
+      <br />
+      <br />
       <input {...register("content", { required: true })}  className='p-2 border'/>
       {errors.exampleRequired && <span className='text-red-300'>please enter again </span>}
+      <br /><br />
       <input type="submit"  className='bg-teal-500 text-white'/>
     </form>
      
