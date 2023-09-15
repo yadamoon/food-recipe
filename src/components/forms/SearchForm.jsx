7a282cb8e4601139f05056ba41d07aabb49aa9b2
Yared <yared241993@gmail.com>
2023-09-15 16:58:41 +0300

    updated searchForm css part

--- src/components/forms/SearchForm.jsx
+++ src/components/forms/SearchForm.jsx
@@ -4,11 +4,11 @@ const SearchForm = () => {
   return (
     <div class="left-0 w-full  z-40 select-none">
       <div class="w-1/3  relative mx-auto mt-10">
-        <div class="bg-white w-full h-16 rounded-xl mb-3 shadow-t p-2  grid-cols-2 flex">
+        <div class="bg-white w-full h-16 rounded-xl mb-3 shadow-t p-2  grid-cols-2 flex sm:w-full">
           <input
             type="text"
             placeholder="Search"
-            className="w-full p-3 rounded"
+            className="w-full p-3 rounded "
           />
           <span></span>
         </div>
