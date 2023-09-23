ba893fa74be32e1597f2aa84ba7992963f3a5ab7
Yared <yared241993@gmail.com>
2023-09-23 14:52:02 +0300

    add button

--- src/components/forms/SearchForm.jsx
+++ src/components/forms/SearchForm.jsx
@@ -3,14 +3,16 @@ import React from 'react'
 const SearchForm = () => {
   return (
     <div class=" flex justify-center">
-      <div class=" grid grid-cols-1">
-        <div class="md:col-span-1 lg:col-span-1">
+      <div class=" grid grid-cols-4 gap-2">
+        <div class="md:col-span-3 lg:col-span-3 col-span-3 g">
           <input
             type="text"
             placeholder="Search"
-            className="w-full p-3 rounded "
+            className="w-full p-3 rounded-l "
           />
-          <span></span>
+        </div>
+        <div className="col-span-1">
+          <button className="border p-3 bg-white rounded">Search</button>
         </div>
       </div>
     </div>
