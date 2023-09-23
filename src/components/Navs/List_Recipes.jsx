6c2687a0ad3c925f7cca5b1bc99a89bc5681f78b
Yared <yared241993@gmail.com>
2023-09-23 15:43:42 +0300

    updated css part

--- src/components/Navs/List_Recipes.jsx
+++ src/components/Navs/List_Recipes.jsx
@@ -27,51 +27,45 @@ function DetailsComponents() {
   }
   return (
     <div>
-      <h1 className="text-5xl text-center font-extrabold text-teal-800">
+      <h1 className="text-3xl text-center font-extrabold text-teal-800">
         Our Food Recipes
       </h1>
 
       <SearchForm />
-      <div className="w-full  flex items-center justify-center">
-        <div className=" grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-8 sm:grid-cols-1 p-5">
+      <div className="">
+        <div className="  grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8 sm:grid-cols-2 p-3 ">
           {recipes.map((recipe, index) => (
-            <section
+            <div
               className=" h-5/5 body-font bg-white text-gray-600 rounded-t"
               key={recipe.id}
             >
-              <div className="container grid grid-cols">
-                <div className=" flex flex-wrap">
-                  <div className="">
-                    <Link
-                      to={'/recipes/' + recipe._id}
-                      className="relative block h-48 w-full overflow-hidden rounded-t"
-                      // onClick={<DetailsComponents id={recipe._id} />}
-                      // onClick={() => navigate('recipes/' + recipe._id)}
-                    >
-                      <img
-                        alt="ecommerce"
-                        className="block h-full w-full object-cover object-center cursor-pointer"
-                        src="https://dummyimage.com/421x261"
-                      />
-                    </Link>
-                    <div className="mt-4">
-                      <h3 className="text-lg tracking-widest text-gray-500 font-bold">
-                        {recipe.title}
-                      </h3>
-                      <h2 className="title-font text-2sm font-medium text-gray-900">
-                        {recipe.body}
-                      </h2>
+              <div className="">
+                <Link
+                  to={'/recipes/' + recipe._id}
+                  className="relative block h-auto w-full overflow-hidden rounded-t"
+                >
+                  <img
+                    alt="recipe"
+                    className="block h-full w-full object-cover object-center cursor-pointer hover:opacity-75"
+                    src="public/images/spageti.png"
+                  />
+                </Link>
+                <div className="mt-4 w-full ml-2">
+                  <h3 className="text-lg tracking-widest text-gray-500 font-bold">
+                    {recipe.title}
+                  </h3>
+                  <h2 className="title-font text-gray-500 font-medium">
+                    {recipe.body}
+                  </h2>
 
-                      <p className="mt-1">{ago(new Date())}</p>
-                    </div>
-                  </div>
+                  <p className="from-neutral-400 text-sm ">{ago(new Date())}</p>
                 </div>
               </div>
-            </section>
+            </div>
           ))}
         </div>
       </div>
-      <div className="flex justify-center">
+      <div className="flex justify-center space-y-2">
         <Pagination />
       </div>
     </div>
@@ -88,7 +82,7 @@ export function Pagination() {
           <li>
             <a
               href="#"
-              class="bg-white border border-gray-300 text-gray-500  hover:text-gray-700 ml-0 rounded-l-lg leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              class="bg-white border border-gray-300 text-gray-500  hover:text-gray-700 ml-0 rounded-l-lg leading-tight py-2 px-3   dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
             >
               Previous
             </a>
@@ -96,7 +90,7 @@ export function Pagination() {
           <li>
             <a
               href="#"
-              class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3  dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
             >
               1
             </a>
@@ -104,7 +98,7 @@ export function Pagination() {
           <li>
             <a
               href="#"
-              class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3   dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
             >
               2
             </a>
@@ -113,7 +107,7 @@ export function Pagination() {
             <a
               href="#"
               aria-current="page"
-              class="bg-blue-50 border border-gray-300 text-blue-600 hover:bg-blue-100 hover:text-blue-700  py-2 px-3 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
+              class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3  dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
             >
               3
             </a>
@@ -121,7 +115,7 @@ export function Pagination() {
           <li>
             <a
               href="#"
-              class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3  dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
             >
               4
             </a>
@@ -129,7 +123,7 @@ export function Pagination() {
           <li>
             <a
               href="#"
-              class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 leading-tight py-2 px-3  dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
             >
               5
             </a>
@@ -137,7 +131,7 @@ export function Pagination() {
           <li>
             <a
               href="#"
-              class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 rounded-r-lg leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 rounded-r-lg leading-tight py-2 px-3  dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
             >
               Next
             </a>
