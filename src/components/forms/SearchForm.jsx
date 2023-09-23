b3e6b7f7812831c8123338331036275a712ed65b
Yared <yared241993@gmail.com>
2023-09-23 14:37:01 +0300

    updated screen size

--- src/components/forms/SearchForm.jsx
+++ src/components/forms/SearchForm.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const SearchForm = () => {
   return (
-    <div class="left-0 w-full  z-40 select-none">
-      <div class="w-1/3  relative mx-auto mt-10">
-        <div class="bg-white w-full h-16 rounded-xl mb-3 shadow-t p-2  grid-cols-2 flex sm:w-full">
+    <div class=" flex justify-center">
+      <div class=" grid grid-cols-1">
+        <div class="md:col-span-1 lg:col-span-1">
           <input
             type="text"
             placeholder="Search"
