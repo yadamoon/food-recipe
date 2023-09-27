a56557f19b52523d50d3d4e8f25a616eb7c14baa
Yared <yared241993@gmail.com>
2023-09-27 12:12:42 +0300

    sth updatd

--- src/components/forms/SearchForm.jsx
+++ src/components/forms/SearchForm.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const SearchForm = () => {
+const SearchForm = (recipes) => {
+  const [search, setSearch] = useState()
+
+  const SearchHandel = ({ recipes }) => {
+    console.log('Hello')
+    console.log(recipes)
+    console.log(search)
+    return recipes.filter((item)=>
+    keys.some(key => item[key].toLowerCase().includes(search))
+    )
+  }
   return (
     <div class=" flex justify-center">
       <div class=" grid grid-cols-4 gap-2">
@@ -9,10 +19,18 @@ const SearchForm = () => {
             type="text"
             placeholder="Search"
             className="w-full p-3 rounded-l "
+            onChange={(e) => {
+              setSearch(e.target.value)
+            }}
           />
         </div>
         <div className="col-span-1">
-          <button className="border p-3 bg-white rounded">Search</button>
+          <button
+            className="border p-3 bg-white rounded"
+            onClick={SearchHandel}
+          >
+            Search
+          </button>
         </div>
       </div>
     </div>
