92f243f0bc1638cfb2f648d3c880b3eadd1d347f
Yared <yared241993@gmail.com>
2023-09-28 14:27:27 +0300

    add new Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,47 +7,65 @@ const Nav = () => {
 
   return (
     <div>
-      <nav className=" bg-white p-5 ">
-        <ul className="flex w-full justify-center  sm:text-sm items-center space-x-4 uppercase">
+      <nav className="  bg-white p-5 ">
+        <ul className="flex w-full justify-center  text-sm items-center space-x-4 uppercase">
           <li>
             <Link
               to="/"
-              className="text-md font-semibold text-gray-600 hover:text-gray-800"
+              className="text-md font-semibold text-gray-600 hover:text-gray-800 border rounded p-2 hover:border-gray-400"
             >
               Home
             </Link>
           </li>
-          <li>
-            <Link
-              to="/listRecipe"
-              className="text-md font-semibold text-gray-600 hover:text-gray-800"
-            >
-              Recipes
-            </Link>
-          </li>
-
-          <li>
-            <Link
-              to="/Add_New_Recipe"
-              className="text-md font-semibold text-gray-600 hover:text-gray-800"
-            >
-              Add
-            </Link>
-          </li>
-
+          {signedIn && (
+            <li>
+              <Link
+                to="/listRecipe"
+                className="text-md font-semibold text-gray-600 hover:text-gray-800 border rounded p-2 hover:border-gray-400"
+              >
+                Recipes
+              </Link>
+            </li>
+          )}{' '}
+          {signedIn && (
+            <li>
+              <Link
+                to="/Add_New_Recipe"
+                className="text-md font-semibold text-gray-600 hover:text-gray-800 border rounded p-2 hover:border-gray-400"
+              >
+                Add
+              </Link>
+            </li>
+          )}
           <li>
             <Link
               to="/settings"
-              className="text-md font-semibold text-gray-600 hover:text-gray-800"
+              className="text-md font-semibold text-gray-600 hover:text-gray-800 border rounded p-2 hover:border-gray-400"
             >
               Settings
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about"
+              className="text-md font-semibold text-gray-600 hover:text-gray-800 border rounded p-2 hover:border-gray-400"
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/contact"
+              className="text-md font-semibold text-gray-600 hover:text-gray-800 border rounded p-2 hover:border-gray-400 "
+            >
+              contact
             </Link>
           </li>
           {signedIn && (
             <li>
               <Link
                 to="/help"
-                className="text-md font-semibold text-gray-600 hover:text-gray-800"
+                className="text-md font-semibold text-gray-600 hover:text-gray-800 border rounded p-2 hover:border-gray-400"
               >
                 Log Out
               </Link>
