e9a44363f5dad14e7f971dc61f3c020a42a122f1
Yared <yared241993@gmail.com>
2023-09-28 15:13:59 +0300

    creation new component

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,6 @@ function App() {
       <ToastContainer />
       <HeaderApp />
       <Nav />
-      <br />
 
       <Routes className="pl-40 pr-40">
         <Route path="/" element={<Home />} />
