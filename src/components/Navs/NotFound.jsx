25815ad7d1d08a7fdbe2a694c9507c2d5be57453
Yared <yared241993@gmail.com>
2023-09-29 12:23:02 +0300

    updated a tag to Link and set Path of Links to home(/)

--- src/components/Navs/NotFound.jsx
+++ src/components/Navs/NotFound.jsx
@@ -15,12 +15,12 @@ const NotFound = () => {
             Sorry, we couldn’t find the page you’re looking for.
           </p>
           <div class="mt-10 flex items-center justify-center gap-x-6">
-            <a
-              href="#"
+            <Link
+              to="/"
               class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Go back home
-            </a>
+            </Link>
             <a href="#" class="text-sm font-semibold text-gray-900">
               Contact support <Link to="/Contact">&rarr;</Link>
             </a>
