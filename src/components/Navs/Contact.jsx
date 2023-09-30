0b7c65239da3ed4c2e49ca3cba2269c18e7761fe
Yared <yared241993@gmail.com>
2023-09-30 14:47:04 +0300

    updated sth

--- src/components/Navs/Contact.jsx
+++ src/components/Navs/Contact.jsx
@@ -3,15 +3,30 @@ import React from 'react'
 const Contact = () => {
   return (
     <div className="flex justify-center">
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8 sm:grid-cols-2 p-">
-        <div className=" h-5/5 body-font bg-white text-gray-600 rounded-t">
-          Call Us
+      <div className="">
+        <h1>Contact Us</h1>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8 sm:grid-cols-2 w-2/3">
+        <div className=" h-auto body-font bg-white text-gray-600 rounded-t text-center">
+          <h1 className="text-2xl text-center text-teal-800"> Call Us</h1>
+          <p>+251-932380202</p>
+        </div>
+        <div className=" h-28  body-font bg-white text-gray-600 rounded-t">
+          <h1 className="text-2xl text-center text-teal-800"> Chat Live</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</p>
+          <button className="border rounded bg-green-800 text-white justify-center">
+            Chat Live
+          </button>
         </div>
         <div className=" h-5/5 body-font bg-white text-gray-600 rounded-t">
-          Chat Live
-        </div>
-        <div className=" h-5/5 body-font bg-white text-gray-600 rounded-t">
-          Ask a Question
+          <h1 className="text-2xl text-center text-teal-800">
+            {' '}
+            Ask a Question
+          </h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum</p>
+          <button className="border rounded bg-green-800 text-white ">
+            Chat Live
+          </button>
         </div>
       </div>
     </div>
