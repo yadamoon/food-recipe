175fe3609734c09114ae40eba988affdf173e72c
Yared <yared241993@gmail.com>
2023-09-30 15:27:27 +0300

    sth updated

--- src/components/Navs/Contact.jsx
+++ src/components/Navs/Contact.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className="flex bg-white">
+    <div className="flex ">
       <div>
         <h1 className="">Contact US</h1>
       </div>
       <br />
       <div className="flex justify-center">
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8 sm:grid-cols-2 w-2/3">
-          <div className=" h-auto body-font bg-white text-gray-600 rounded-t space-y-3 text-center">
+          <div className=" h-auto body-font bg-gray-100 text-gray-600 rounded-t space-y-3 text-center">
             <img
               src="public/svg/call-svgrepo-com.svg"
               width={60}
@@ -23,7 +23,7 @@ const Contact = () => {
             </h1>
             <p>+251-932380202</p>
           </div>
-          <div className=" h-auto  body-font bg-white text-gray-600 rounded-t space-y-3">
+          <div className=" h-auto  body-font bg-gray-100 text-gray-600 rounded-t space-y-3">
             <img
               src="public/svg/chat-round-line-svgrepo-com.svg"
               width={60}
@@ -41,7 +41,7 @@ const Contact = () => {
               Chat Live
             </button>
           </div>
-          <div className=" h-auto body-font bg-white text-gray-600 rounded-t space-y-3">
+          <div className=" h-auto body-font bg-gray-100 text-gray-600 rounded-t space-y-3">
             <img
               src="public/svg/ask-svgrepo-com.svg"
               width={60}
