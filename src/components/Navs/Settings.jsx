c4f7afc25865d7ee4865b99327a9b071669a2b57
Yared <yared241993@gmail.com>
2023-10-03 18:02:10 +0300

    updated css part

--- src/components/Navs/Settings.jsx
+++ src/components/Navs/Settings.jsx
@@ -33,8 +33,8 @@ const Settings = () => {
             <a
               key={index}
               className={
-                (index === activeMenu ? 'text-teal-700' : '') +
-                ' inline-flex items-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:text-accent-foreground h-9 px-4 py-2 hover:bg-transparent hover:underline justify-start w-full border'
+                (index === activeMenu ? 'text-white bg-teal-700' : '') +
+                ' inline-flex items-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50  h-9 px-4 py-2 justify-start w-full border'
               }
               onClick={() => setActiveMenu(index)}
             >
