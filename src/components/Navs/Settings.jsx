389a5fe2d1b4d8fd54b26ac65cbde0c245cc2562
Yared <yared241993@gmail.com>
2023-10-03 17:57:56 +0300

    updated code for simple understand

--- src/components/Navs/Settings.jsx
+++ src/components/Navs/Settings.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Settings = () => {
+  const contents = [
+    {
+      title: 'Public Profile',
+      content: publicProfile,
+    },
+    {
+      title: 'Account Settings',
+      content: accountSettings,
+    },
+    {
+      title: 'Account Settings',
+      content: notifications,
+    },
+  ]
+  const [activeMenu, setActiveMenu] = useState(0)
+  const ActiveContent = contents[activeMenu].content
   return (
     <div className="hidden space-y-6 p-10 pb-16 md:block bg-white">
       <div className="space-y-0.5">
@@ -13,29 +29,34 @@ const Settings = () => {
       <div className="shrink-0 bg-border h-[1px] w-full"></div>
       <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
         <nav className="flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1">
-          <a className="inline-flex items-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:text-accent-foreground h-9 px-4 py-2 hover:bg-transparent hover:underline justify-start w-full border">
-            Public Profile
-          </a>
-          <a className="inline-flex items-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:text-accent-foreground h-9 px-4 py-2 bg-muted hover:bg-muted justify-start w-full border">
-            Account Settings
-          </a>
-
-          <a className="inline-flex items-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:text-accent-foreground h-9 px-4 py-2 hover:bg-transparent hover:underline justify-start w-full border">
-            Notifications
-          </a>
+          {contents.map((item, index) => (
+            <a
+              key={index}
+              className={
+                (index === activeMenu ? 'text-teal-700' : '') +
+                ' inline-flex items-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:text-accent-foreground h-9 px-4 py-2 hover:bg-transparent hover:underline justify-start w-full border'
+              }
+              onClick={() => setActiveMenu(index)}
+            >
+              {item.title}
+            </a>
+          ))}
         </nav>
 
-        <div className="flex-1 lg:max-w-2xl">
-          {/* <!-- About Page Content --> */}
-          <h2 className="text-lg font-bold tracking-tight">Account</h2>
-          <p className="text-sm text-muted-foreground">
-            Update your account settings. Set your preferred language and
-            timezone.
-          </p>
-        </div>
+        <ActiveContent />
       </div>
     </div>
   )
 }
 
 export default Settings
+
+const publicProfile = () => {
+  return <div className=""> publicProfile</div>
+}
+const accountSettings = () => {
+  return <div className="">accountSettings</div>
+}
+const notifications = () => {
+  return <div className="">notifications</div>
+}
