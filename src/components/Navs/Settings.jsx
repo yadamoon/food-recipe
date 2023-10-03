28b46750a2b0fbe292591747d7fa6c1f3e78d05e
Yared <yared241993@gmail.com>
2023-10-03 18:24:50 +0300

    full updated singoiut function

--- src/components/Navs/Settings.jsx
+++ src/components/Navs/Settings.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import auth from '../../services/http/auth'
 
 const Settings = () => {
   const contents = [
@@ -17,6 +18,9 @@ const Settings = () => {
   ]
   const [activeMenu, setActiveMenu] = useState(0)
   const ActiveContent = contents[activeMenu].content
+  const handlLogout = () => {
+    auth.signOut()
+  }
   return (
     <div className="hidden space-y-6 p-10 pb-16 md:block bg-white">
       <div className="space-y-0.5">
@@ -45,6 +49,7 @@ const Settings = () => {
 
         <ActiveContent />
       </div>
+      <button onClick={handlLogout}>logout</button>
     </div>
   )
 }
