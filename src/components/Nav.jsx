fafdce33dd47b4a5e34b33be960da5e78a85d85f
Yared <yared241993@gmail.com>
2023-10-13 15:26:10 +0300

    deleted unecessary code

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,77 +8,10 @@ const Nav = () => {
 
   return (
     <div className="">
-      {/* <nav className="  bg-white p-5 ">
-        <ul className="flex w-full justify-center  text-sm items-center space-x-4 uppercase">
-          <li>
-            <Link
-              to="/"
-              className="text-md font-semibold text-gray-600 hover:text-gray-800 border rounded p-2 hover:border-gray-400"
-            >
-              Home
-            </Link>
-          </li>
-          {signedIn && (
-            <li>
-              <Link
-                to="/listRecipe"
-                className="text-md font-semibold text-gray-600 hover:text-gray-800 border rounded p-2 hover:border-gray-400"
-              >
-                Recipes
-              </Link>
-            </li>
-          )}{' '}
-          {signedIn && (
-            <li>
-              <Link
-                to="/Add_New_Recipe"
-                className="text-md font-semibold text-gray-600 hover:text-gray-800 border rounded p-2 hover:border-gray-400"
-              >
-                Add
-              </Link>
-            </li>
-          )}
-          <li>
-            <Link
-              to="/settings"
-              className="text-md font-semibold text-gray-600 hover:text-gray-800 border rounded p-2 hover:border-gray-400"
-            >
-              Settings
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/about"
-              className="text-md font-semibold text-gray-600 hover:text-gray-800 border rounded p-2 hover:border-gray-400"
-            >
-              About
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/contact"
-              className="text-md font-semibold text-gray-600 hover:text-gray-800 border rounded p-2 hover:border-gray-400 "
-            >
-              contact
-            </Link>
-          </li>
-          {signedIn && (
-            <li>
-              <Link
-                to="/help"
-                className="text-md font-semibold text-gray-600 hover:text-gray-800 border rounded p-2 hover:border-gray-400"
-              >
-                Log Out
-              </Link>
-            </li>
-          )}
-        </ul>
-      </nav> */}
-
       <div>
         <div className=" bg-white rounded "></div>
         <div className=" xl:mx-0 h-12   bg-white shadow rounded">
-          <ul className="flex border-b px-5  justify-end ">
+          <ul className="flex border-b px-5  justify-end  ">
             <li
               onClick={() => setActiveStatus(1)}
               className={
