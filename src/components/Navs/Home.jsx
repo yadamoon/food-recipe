bf052dc1e35af20de668772b2b38f49df257f105
Yared <yared241993@gmail.com>
2023-10-18 14:30:06 +0300

    add piopular video cards

--- src/components/Navs/Home.jsx
+++ src/components/Navs/Home.jsx
@@ -4,49 +4,194 @@ export default function Home() {
   return (
     <div className="spacey-10">
       <div className="bg-gradient-to-br from-gray-100 to-gray-200 min-h-screen overflow-auto">
-        <div className="container max-w-5xl mx-auto px-4">
-          <div className="w-4/5">
-            <h1 className="mt-32 text-gray-500 text-6xl font-bold">
-              Find Your Tasty and Healthy Food
-              <br />
-              <span className="text-gray-600">
-                on this Food Recipe Application.
-              </span>
-            </h1>
+        {/* //!? from tilawind Componenets */}
+        <div className=" 2xl:container 2xl:mx-auto md:py-12 lg:px-20 md:px-6 py-9 px-4">
+          <div className=" text-center">
+            <h2 className=" font-semibold lg:text-4xl text-3xl lg:leading-9 md:leading-7 leading-9 text-gray-800 md:w-full w-9/12 mx-auto">
+              Follow Us on Instagram
+            </h2>
+            <p className=" font-normal text-base leading-6 text-gray-600 mt-4 lg:w-5/12 md:w-9/12 mx-auto">
+              Follow us on instagram @
+              <span className="underline cursor-pointer">followuspleaseee</span>{' '}
+              and tag us to get featured on our timeline
+            </p>
           </div>
-          <div className="w-5/6 my-10 ml-6">
-            <h3 className="text-gray-600">
-              Create, edit & deploy fullstack apps with <br />
-              <strong className="">
-                faster package installations & greater security
-              </strong>
-              <br />
-              than even local environments.
-            </h3>
-          </div>
-          <div className="hidden sm:block opacity-50 z-0">
-            <div className="shadow-2xl w-96 h-96 rounded-full -mt-72 bg-teal-700"></div>
-            <div className="shadow-2xl w-96 h-96 rounded-full -mt-96 bg-teal-200"></div>
-            <div className="shadow-xl w-80 h-80 rounded-full ml-8 -mt-96 bg-teal-400"></div>
-          </div>
-          <div className=" space-y-3 grid grid-cols-3 gap-2">
-            <div className="border rounded">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis,
-              rem. In reiciendis ducimus molestias laboriosam accusantium minus
-              id vero distinctio eaque facere corrupti dolores excepturi, beatae
-              optio illo soluta tempora.
+          <div className=" grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 lg:grap-8 md:gap-6 gap-4 mt-10">
+            <div className="relative group">
+              <img
+                src="https://i.ibb.co/QHS8Ngp/pexels-alana-sousa-3294250-1.png"
+                alt="A picture of a sitting dog"
+                className=" lg:block hidden w-full "
+              />
+              <img
+                src="https://i.ibb.co/mNPBgQN/pexels-alana-sousa-3294250-1-1.png"
+                alt="A picture of a sitting dog"
+                className="lg:hidden block w-full "
+              />
+              <div className=" flex justify-center items-center opacity-0 bg-gradient-to-t from-gray-800 via-gray-800 to-opacity-30 group-hover:opacity-50 absolute top-0 left-0 h-full w-full" />
+              <div className=" absolute top-0 left-0 w-full h-full flex justify-center items-center opacity-0 hover:opacity-100">
+                <svg
+                  width={64}
+                  height={64}
+                  viewBox="0 0 64 64"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M42.6665 10.6665H21.3332C15.4421 10.6665 10.6665 15.4421 10.6665 21.3332V42.6665C10.6665 48.5575 15.4421 53.3332 21.3332 53.3332H42.6665C48.5575 53.3332 53.3332 48.5575 53.3332 42.6665V21.3332C53.3332 15.4421 48.5575 10.6665 42.6665 10.6665Z"
+                    stroke="white"
+                    strokeWidth="2.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M32 40C36.4183 40 40 36.4183 40 32C40 27.5817 36.4183 24 32 24C27.5817 24 24 27.5817 24 32C24 36.4183 27.5817 40 32 40Z"
+                    stroke="white"
+                    strokeWidth="2.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M44 20V20.001"
+                    stroke="white"
+                    strokeWidth="2.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </div>
             </div>
-            <div className="border border-teal-200 rounded ">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-              provident expedita architecto officia quia consequuntur
-              reprehenderit harum iusto. Rem unde est repellat ea delectus
-              consequuntur tenetur ipsum eaque eum sit?{' '}
+            <div className="relative group">
+              <img
+                src="https://i.ibb.co/T8jgRy3/pexels-leah-kelley-1449667-1.png"
+                alt="Smiling Girl"
+                className=" lg:block hidden w-full "
+              />
+              <img
+                src="https://i.ibb.co/YD8nNMR/pexels-leah-kelley-1449667-1-1.png"
+                alt="Smiling Girl"
+                className="lg:hidden block w-full "
+              />
+              <div className="opacity-0 bg-gradient-to-t from-gray-800 via-gray-800 to-opacity-30 group-hover:opacity-50 absolute top-0 left-0 h-full w-full" />
+              <div className=" absolute top-0 left-0 w-full h-full flex justify-center items-center opacity-0 hover:opacity-100">
+                <svg
+                  width={64}
+                  height={64}
+                  viewBox="0 0 64 64"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M42.6665 10.6665H21.3332C15.4421 10.6665 10.6665 15.4421 10.6665 21.3332V42.6665C10.6665 48.5575 15.4421 53.3332 21.3332 53.3332H42.6665C48.5575 53.3332 53.3332 48.5575 53.3332 42.6665V21.3332C53.3332 15.4421 48.5575 10.6665 42.6665 10.6665Z"
+                    stroke="white"
+                    strokeWidth="2.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M32 40C36.4183 40 40 36.4183 40 32C40 27.5817 36.4183 24 32 24C27.5817 24 24 27.5817 24 32C24 36.4183 27.5817 40 32 40Z"
+                    stroke="white"
+                    strokeWidth="2.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M44 20V20.001"
+                    stroke="white"
+                    strokeWidth="2.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </div>
             </div>
-            <div className="border rounded">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
-              necessitatibus corporis voluptatem! Eius eum praesentium quos
-              dicta rerum perspiciatis fugit earum sit cupiditate. Vero ad,
-              provident pariatur eligendi voluptate minima.
+            <div className="relative group">
+              <img
+                src="https://i.ibb.co/F3dzNWD/pexels-spencer-selover-775358-1.png"
+                alt="Men Posing"
+                className=" lg:block hidden w-full "
+              />
+              <img
+                src="https://i.ibb.co/myWxfSm/pexels-spencer-selover-775358-1-1.png"
+                alt="Men Posing"
+                className="lg:hidden block w-full "
+              />
+              <div className="opacity-0 bg-gradient-to-t from-gray-800 via-gray-800 to-opacity-30 group-hover:opacity-50 absolute top-0 left-0 h-full w-full" />
+              <div className=" absolute top-0 left-0 w-full h-full flex justify-center items-center opacity-0 hover:opacity-100">
+                <svg
+                  width={64}
+                  height={64}
+                  viewBox="0 0 64 64"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M42.6665 10.6665H21.3332C15.4421 10.6665 10.6665 15.4421 10.6665 21.3332V42.6665C10.6665 48.5575 15.4421 53.3332 21.3332 53.3332H42.6665C48.5575 53.3332 53.3332 48.5575 53.3332 42.6665V21.3332C53.3332 15.4421 48.5575 10.6665 42.6665 10.6665Z"
+                    stroke="white"
+                    strokeWidth="2.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M32 40C36.4183 40 40 36.4183 40 32C40 27.5817 36.4183 24 32 24C27.5817 24 24 27.5817 24 32C24 36.4183 27.5817 40 32 40Z"
+                    stroke="white"
+                    strokeWidth="2.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M44 20V20.001"
+                    stroke="white"
+                    strokeWidth="2.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div className="relative group">
+              <img
+                src="https://i.ibb.co/DwcwgDP/pexels-chevanon-photography-1108099-1.png"
+                alt="2 puppies"
+                className=" lg:block hidden w-full "
+              />
+              <img
+                src="https://i.ibb.co/5cDQZ2r/pexels-chevanon-photography-1108099-1-1.png"
+                alt="2 puppies"
+                className="lg:hidden block w-full "
+              />
+              <div className="opacity-0 bg-gradient-to-t from-gray-800 via-gray-800 to-opacity-30 group-hover:opacity-50 absolute top-0 left-0 h-full w-full" />
+              <div className=" absolute top-0 left-0 w-full h-full flex justify-center items-center opacity-0 hover:opacity-100">
+                <svg
+                  width={64}
+                  height={64}
+                  viewBox="0 0 64 64"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M42.6665 10.6665H21.3332C15.4421 10.6665 10.6665 15.4421 10.6665 21.3332V42.6665C10.6665 48.5575 15.4421 53.3332 21.3332 53.3332H42.6665C48.5575 53.3332 53.3332 48.5575 53.3332 42.6665V21.3332C53.3332 15.4421 48.5575 10.6665 42.6665 10.6665Z"
+                    stroke="white"
+                    strokeWidth="2.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M32 40C36.4183 40 40 36.4183 40 32C40 27.5817 36.4183 24 32 24C27.5817 24 24 27.5817 24 32C24 36.4183 27.5817 40 32 40Z"
+                    stroke="white"
+                    strokeWidth="2.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M44 20V20.001"
+                    stroke="white"
+                    strokeWidth="2.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </div>
             </div>
           </div>
         </div>
