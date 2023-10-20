1f7b151544d5290ebddee90b5e2f23e3b10eaae3
Yared <yared241993@gmail.com>
2023-10-20 13:54:00 +0300

    updated sth

--- src/components/forms/ContactForm.jsx
+++ src/components/forms/ContactForm.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
-import { Form } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 
 const ContactForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+  const handleClick = ({ name, phone, email, message }) => {
+    console.log(`${name},${phone},${email},${message}`)
+  }
   return (
     <div className="w-full h-screen">
       <div className=" h-96"></div>
@@ -16,27 +24,95 @@ const ContactForm = () => {
                 <label className="font-semibold leading-none text-gray-600">
                   Name
                 </label>
+
                 <input
-                  type="text"
-                  className="leading-none text-gray-50 p-3  focus:teal-blue-700 mt-4 border rounded"
+                  {...register('name', {
+                    required: {
+                      value: true,
+                      message: 'Please Enter Your Name!',
+                    },
+                    minLength: {
+                      value: 6,
+                      message: 'min length are at least 6',
+                    },
+                    maxLength: {
+                      value: 32,
+                      message: 'max length are at least 32',
+                    },
+                  })}
+                  placeholder=" Name"
+                  className="text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-indigo-700 mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100"
                 />
+                {errors.name && (
+                  <span className="text-red-700 col-span-1 ">
+                    {errors.name.message}
+                  </span>
+                )}
               </div>
               <div className="w-full md:w-1/2 flex flex-col md:ml-6 md:mt-0 mt-4">
                 <label className="font-semibold leading-none text-gray-600">
                   Phone
                 </label>
-                <input type="text" className=" p-3 mt-4 border  rounded" />
+                <input />
+                <input
+                  {...register('phone', {
+                    required: {
+                      value: true,
+                      message: 'Please Enter Your Phone Number!',
+                    },
+                    minLength: {
+                      value: 6,
+                      message: 'min length are at least 6',
+                    },
+                    maxLength: {
+                      value: 32,
+                      message: 'max length are at least 32',
+                    },
+                  })}
+                  placeholder="Phone Number"
+                  className=" p-3 mt-4 border  rounded"
+                />
+                {errors.phone && (
+                  <span className="text-red-700 col-span-1 ">
+                    {errors.phone.message}
+                  </span>
+                )}
               </div>
             </div>
+
             <div className="md:flex items-center mt-8">
               <div className="w-full flex flex-col">
                 <label className="font-semibold leading-none text-gray-600">
                   Email
                 </label>
+
                 <input
-                  type="text"
+                  {...register('email', {
+                    required: {
+                      value: true,
+                      message: 'Please Enter Your Phone Number!',
+                    },
+                    minLength: {
+                      value: 6,
+                      message: 'min length are at least 6',
+                    },
+                    maxLength: {
+                      value: 32,
+                      message: 'max length are at least 32',
+                    },
+                    pattern: {
+                      value: /[@]/,
+                      message: 'Enter at least one special character',
+                    },
+                  })}
+                  placeholder=" Email"
                   className="leading-none  p-3  mt-4 border  rounded"
                 />
+                {errors.email && (
+                  <span className="text-red-700 col-span-1 ">
+                    {errors.email.message}
+                  </span>
+                )}
               </div>
             </div>
             <div>
@@ -46,12 +122,35 @@ const ContactForm = () => {
                 </label>
                 <textarea
                   type="text"
+                  {...register('phone', {
+                    required: {
+                      value: true,
+                      message: 'Please Enter Your Phone Number!',
+                    },
+                    minLength: {
+                      value: 6,
+                      message: 'min length are at least 6',
+                    },
+                    maxLength: {
+                      value: 32,
+                      message: 'max length are at least 32',
+                    },
+                  })}
+                  placeholder="Phone Number"
                   className="h-40 text-base  focus:teal-blue-700 mt-4  border rounded"
-                ></textarea>
+                />
+                {errors.phone && (
+                  <span className="text-red-700 col-span-1 ">
+                    {errors.phone.message}
+                  </span>
+                )}
               </div>
             </div>
             <div className="flex items-center justify-center w-full">
-              <button className="mt-9 font-semibold leading-none text-white py-4 px-10 bg-teal-700 rounded hover:bg-teal-600 focus:ring-2 focus:ring-offset-2 focus:ring-teal-700 focus:outline-none">
+              <button
+                onClick={handleSubmit(handleClick)}
+                className="mt-9 font-semibold leading-none text-white py-4 px-10 bg-teal-700 rounded hover:bg-teal-600 focus:ring-2 focus:ring-offset-2 focus:ring-teal-700 focus:outline-none"
+              >
                 Send message
               </button>
             </div>
