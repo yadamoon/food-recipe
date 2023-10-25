697b1eb0946b8032c240e31d4dee0c0a92406050
Yared <yared241993@gmail.com>
2023-10-25 16:38:16 +0300

    add new form

--- src/components/Navs/Settings.jsx
+++ src/components/Navs/Settings.jsx
@@ -22,15 +22,15 @@ const Settings = () => {
     auth.signOut()
   }
   return (
-    <div className="hidden space-y-6 p-10 pb-16 md:block bg-white mx-5 rounded justify-center">
-      <div className="space-y-0.5">
+    <div className="">
+      <div>
         <h2 className="text-2xl font-bold tracking-tight text-center">
           Settings
         </h2>
       </div>
       {/* <!-- Seperator --> */}
-      <div className="grid grid-cols-3 gap-4">
-        <div className="shrink-0 bg-border h-[1px] w-full col-span-3"></div>
+      <div>
+        <div></div>
         <div className=" ">
           <nav className=" w-2/3">
             {contents.map((item, index) => (
@@ -62,20 +62,20 @@ const Settings = () => {
 export default Settings
 import { useForm } from 'react-hook-form'
 const publicProfile = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm()
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   formState: { errors },
+  // } = useForm()
 
-  const saveToDB = (data) => {
-    console.log(data) // You can handle the form data here
-  }
+  // const saveToDB = (data) => {
+  //   console.log(data) // You can handle the form data here
+  // }
 
   return (
-    <div className="bg-white justify-center flex ml-12">
-      <form action="">
-        <div className=" grid grid-cols-3 ">
+    <div>
+      {/* <form action="">
+        <div className=" ">
           <div className="w-full  col-span-1">
             <label className="font-semibold leading-none text-gray-600">
               First Name
@@ -300,7 +300,167 @@ const publicProfile = () => {
             Save
           </button>
         </div>
-      </form>
+      </form> */}
+
+      {/* test */}
+      <section className="bg-white">
+        <div className="max-w-2xl px-4 py-8 mx-auto lg:py-16">
+          <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-black">
+            Update product
+          </h2>
+          <form action="#">
+            <div className="grid gap-4 mb-4 sm:grid-cols-2 sm:gap-6 sm:mb-5">
+              <div className="sm:col-span-2">
+                <label
+                  for="name"
+                  className="block mb-2 text-sm font-medium  dark:text-black"
+                >
+                  Email
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  required=""
+                />
+              </div>
+              <div className="w-full">
+                <label
+                  for="brand"
+                  class="block mb-2 text-sm font-medium  dark:text-black"
+                >
+                  First Name
+                </label>
+                <input
+                  type="text"
+                  name="brand"
+                  id="brand"
+                  className="bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  required=""
+                />
+              </div>
+              <div className="w-full">
+                <label
+                  for="price"
+                  className="block mb-2 text-sm font-medium  dark:text-black"
+                >
+                  Last Name
+                </label>
+                <input
+                  type="number"
+                  name="price"
+                  id="price"
+                  className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  required=""
+                />
+              </div>
+              <div className="w-full">
+                <label
+                  for="brand"
+                  class="block mb-2 text-sm font-medium  dark:text-black"
+                >
+                  First Name
+                </label>
+                <input
+                  type="text"
+                  name="brand"
+                  id="brand"
+                  className="bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  required=""
+                />
+              </div>
+              <div className="w-full">
+                <label
+                  for="price"
+                  className="block mb-2 text-sm font-medium  dark:text-black"
+                >
+                  Last Name
+                </label>
+                <input
+                  type="number"
+                  name="price"
+                  id="price"
+                  className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  required=""
+                />
+              </div>
+              {/* <div>
+                <label
+                  for="category"
+                  className="block mb-2 text-sm font-medium  dark:text-black"
+                >
+                  Address{' '}
+                </label>
+                <select
+                  id="category"
+                  className="bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
+                >
+                  <option selected="">Electronics</option>
+                  <option value="TV">TV/Monitors</option>
+                  <option value="PC">PC</option>
+                  <option value="GA">Gaming/Console</option>
+                  <option value="PH">Phones</option>
+                </select>
+              </div> */}
+              <div>
+                <label
+                  for="item-weight"
+                  className="block mb-2 text-sm font-medium  dark:text-black"
+                >
+                  Item Weight (kg)
+                </label>
+                <input
+                  type="number"
+                  name="item-weight"
+                  id="item-weight"
+                  className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  required=""
+                />
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  for="description"
+                  class="block mb-2 text-sm font-medium  dark:text-black"
+                >
+                  Description
+                </label>
+                <textarea
+                  id="description"
+                  rows="8"
+                  className="block p-2.5 w-full text-sm rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500  "
+                ></textarea>
+              </div>
+            </div>
+            <div className="flex items-center space-x-4">
+              <button
+                type="submit"
+                class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                Update product
+              </button>
+              <button
+                type="button"
+                className="text-red-600 inline-flex items-center hover:text-white border border-red-600 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
+              >
+                <svg
+                  className="w-5 h-5 mr-1 -ml-1"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                Delete
+              </button>
+            </div>
+          </form>
+        </div>
+      </section>
     </div>
   )
 }
