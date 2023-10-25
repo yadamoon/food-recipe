1ffa98b1f5799df5f41c641346cf3ced0f370221
Yared <yared241993@gmail.com>
2023-10-25 17:13:45 +0300

    updated css part of like resposives

--- src/components/Navs/Settings.jsx
+++ src/components/Navs/Settings.jsx
@@ -22,17 +22,14 @@ const Settings = () => {
     auth.signOut()
   }
   return (
-    <div className="">
+    <div className=" mx-10 rounded bg-white">
       <div>
-        <h2 className="text-2xl font-bold tracking-tight text-center">
-          Settings
-        </h2>
+        <h2 className="text-center text-3xl">Settings</h2>
       </div>
       {/* <!-- Seperator --> */}
-      <div>
-        <div></div>
-        <div className=" ">
-          <nav className=" w-2/3">
+      <div className="grid grid-cols-3 gap-3  ">
+        <div className=" inline-block col-span-1 sm:col-span-3 lg:col-span-1 bg-white">
+          <nav className="">
             {contents.map((item, index) => (
               <a
                 key={index}
@@ -53,7 +50,8 @@ const Settings = () => {
             </button>
           </nav>
         </div>
-        <ActiveContent />
+
+        <ActiveContent className="col-span-3 md:col-span-3 sm:col-span-1 " />
       </div>
     </div>
   )
@@ -74,235 +72,6 @@ const publicProfile = () => {
 
   return (
     <div>
-      {/* <form action="">
-        <div className=" ">
-          <div className="w-full  col-span-1">
-            <label className="font-semibold leading-none text-gray-600">
-              First Name
-            </label>
-
-            <input
-              {...register('firstName', {
-                required: {
-                  value: true,
-                  message: 'Please Enter Your First Name!',
-                },
-                minLength: {
-                  value: 2,
-                  message: 'min length are at least 6',
-                },
-                maxLength: {
-                  value: 20,
-                  message: 'max length are at least 32',
-                },
-              })}
-              className=" p-3 mt-4 border  rounded"
-            />
-            {errors.firstName && (
-              <span className="text-red-700 col-span-1 ">
-                {errors.firstName.message}
-              </span>
-            )}
-          </div>
-
-          <div className="w-full   col-span-1">
-            <label className="font-semibold leading-none text-gray-600">
-              Last Name
-            </label>
-            <input
-              {...register('lastName', {
-                required: {
-                  value: true,
-                  message: 'Please Enter Last Name',
-                },
-                minLength: {
-                  value: 2,
-                  message: 'min length are at least 6',
-                },
-                maxLength: {
-                  value: 20,
-                  message: 'max length are at least 32',
-                },
-              })}
-              className=" p-3 mt-4 border  rounded"
-            />
-            {errors.lastName && (
-              <span className="text-red-700 col-span-1 ">
-                {errors.lastName.message}
-              </span>
-            )}
-          </div>
-
-          <div className="w-full  col-span-1">
-            <label className="font-semibold leading-none text-gray-600">
-              Phone
-            </label>
-
-            <input
-              {...register('phone', {
-                required: {
-                  value: true,
-                  message: 'Please Enter Your Phone Number!',
-                },
-                minLength: {
-                  value: 6,
-                  message: 'min length are at least 6',
-                },
-                maxLength: {
-                  value: 32,
-                  message: 'max length are at least 32',
-                },
-              })}
-              className=" p-3 mt-4 border  rounded"
-            />
-            {errors.phone && (
-              <span className="text-red-700 col-span-1 ">
-                {errors.phone.message}
-              </span>
-            )}
-          </div>
-        </div>
-
-        <div className="md:flex items-center mt-8">
-          <div className="w-full flex flex-col">
-            <label className="font-semibold leading-none text-gray-600">
-              Email
-            </label>
-
-            <input
-              {...register('email', {
-                required: {
-                  value: true,
-                  message: 'Please Enter Your Email!',
-                },
-                minLength: {
-                  value: 6,
-                  message: 'min length are at least 6',
-                },
-                maxLength: {
-                  value: 32,
-                  message: 'max length are at least 32',
-                },
-                pattern: {
-                  value: /[@]/,
-                  message: 'Enter at least one special character',
-                },
-              })}
-              className="leading-none  p-4  mt-4 border  rounded"
-            />
-            {errors.email && (
-              <span className="text-red-700 col-span-1 ">
-                {errors.email.message}
-              </span>
-            )}
-          </div>
-        </div>
-        <div>
-          <div className="md:flex items-center mt-12 ">
-            <div className="w-full md:w-1/2 flex flex-col">
-              <label className="font-semibold leading-none text-gray-600">
-                Old Password
-              </label>
-
-              <input
-                type="password"
-                {...register('oldPassword', {
-                  required: {
-                    value: true,
-                    message: 'Please Enter Old Your Password!',
-                  },
-                  minLength: {
-                    value: 6,
-                    message: 'min length are at least 6',
-                  },
-                  maxLength: {
-                    value: 8,
-                    message: 'max length are at least 8',
-                  },
-                })}
-                className=" p-3 mt-4 border  rounded"
-              />
-              {errors.oldPassword && (
-                <span className="text-red-700 col-span-1 ">
-                  {errors.oldPassword.message}
-                </span>
-              )}
-            </div>
-            <div className="w-full md:w-1/2 flex flex-col mx-8">
-              <label className="font-semibold leading-none text-gray-600">
-                New Password
-              </label>
-
-              <input
-                {...register('newPassword', {
-                  required: {
-                    value: true,
-                    message: 'Please Enter New Password!',
-                  },
-                  minLength: {
-                    value: 6,
-                    message: 'min length are at least 6',
-                  },
-                  maxLength: {
-                    value: 8,
-                    message: 'max length are at least 8',
-                  },
-                })}
-                className=" p-3 mt-4 border  rounded"
-              />
-              {errors.newPassword && (
-                <span className="text-red-700 col-span-1 ">
-                  {errors.newPassword.message}
-                </span>
-              )}
-            </div>
-            <div className="w-full md:w-1/2 flex flex-col md:ml-0 md:mt-0 mt-4">
-              <label className="font-semibold leading-none text-gray-600">
-                Confirem Password
-              </label>
-
-              <input
-                {...register('confiremPassword', {
-                  required: {
-                    value: true,
-                    message: 'Please Confirem New Password!',
-                  },
-                  minLength: {
-                    value: 6,
-                    message: 'min length are at least 6',
-                  },
-                  maxLength: {
-                    value: 8,
-                    message: 'max length are at least 8',
-                  },
-                })}
-                className=" p-3 mt-4 border  rounded"
-              />
-              {errors.confiremPassword && (
-                <span className="text-red-700 col-span-1 ">
-                  {errors.confiremPassword.message}
-                </span>
-              )}
-            </div>
-          </div>
-        </div>
-        <div className="grid grid-cols-2 gap-2">
-          <button
-            className="mt-9 font-semibold leading-none text-white py-4 bg-red-700 rounded hover:bg-red-600 focus:ring-2 focus:ring-offset-2 focus:ring-red-700 focus:outline-none col-span-1 "
-            onClick={handleSubmit(saveToDB)}
-          >
-            Don't Save
-          </button>
-          <button
-            className="mt-9 font-semibold leading-none text-white py-4 px-10 bg-teal-700 rounded hover:bg-teal-600 focus:ring-2 focus:ring-offset-2 focus:ring-teal-700 focus:outline-none col-span-1 "
-            onClick={handleSubmit(saveToDB)}
-          >
-            Save
-          </button>
-        </div>
-      </form> */}
-
-      {/* test */}
       <section className="bg-white">
         <div className="max-w-2xl px-4 py-8 mx-auto lg:py-16">
           <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-black">
