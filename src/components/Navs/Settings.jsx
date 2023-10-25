4b00e03f220a17c54cc2980a9a4558ecfc2194cf
Yared <yared241993@gmail.com>
2023-10-25 16:54:26 +0300

    updated button part

--- src/components/Navs/Settings.jsx
+++ src/components/Navs/Settings.jsx
@@ -475,13 +475,7 @@ const publicProfile = () => {
                 />
               </div>
             </div>
-            <div className="flex items-center space-x-4">
-              <button
-                type="submit"
-                class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              >
-                Update product
-              </button>
+            <div className="flex items-center space-x-6 justify-center">
               <button
                 type="button"
                 className="text-red-600 inline-flex items-center hover:text-white border border-red-600 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
@@ -499,6 +493,12 @@ const publicProfile = () => {
                   ></path>
                 </svg>
                 Delete
+              </button>
+              <button
+                type="submit"
+                class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-teal-700"
+              >
+                Update Profile
               </button>
             </div>
           </form>
