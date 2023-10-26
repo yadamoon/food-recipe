25b0c66895b80dfd73bc11d9602d82bbd36c7947
Yared <yared241993@gmail.com>
2023-10-26 13:39:25 +0300

    add img

--- src/components/Navs/Home.jsx
+++ src/components/Navs/Home.jsx
@@ -94,22 +94,22 @@ export const PopularVideo = () => {
   const popularVideo = [
     {
       title: 'pizza',
-      img: 'public/images/burger.png',
-      alt: 'some burger food',
-    },
-    {
-      title: 'Burger',
-      img: 'public/images/bur.png',
-      alt: 'some burger food',
-    },
-    {
-      title: 'shiro',
-      img: 'public/images/bur.png',
+      img: 'public/images/original_italian_pizza.jpg',
       alt: 'some burger food',
     },
     {
       title: 'salad',
-      img: 'public/images/bur.png',
+      img: 'public/images/Screenshot from 2023-09-30 12-00-11.png',
+      alt: 'some burger food',
+    },
+    {
+      title: 'tibs',
+      img: 'public/images/Screenshot from 2023-09-30 12-39-20.png',
+      alt: 'some burger food',
+    },
+    {
+      title: 'shiro',
+      img: 'public/images/Screenshot from 2023-09-27 16-09-25.png',
       alt: 'some burger food',
     },
   ]
@@ -131,7 +131,12 @@ export const PopularVideo = () => {
               className="relative group"
               animate={{ x: 5, y: 10 }}
             >
-              <img src={foods.img} alt={foods.alt} className="  " />
+              <img
+                src={foods.img}
+                alt={foods.alt}
+                className=" rounded w-full "
+              />
+
               <div className=" flex justify-center items-center opacity-0 bg-gradient-to-t from-gray-800 via-gray-800 to-opacity-30 group-hover:opacity-50 absolute top-0 left-0 h-full w-full" />
               <div className=" absolute top-0 left-0 w-full h-full flex justify-center items-center opacity-0 hover:opacity-100">
                 {/* <svg
@@ -163,7 +168,7 @@ export const PopularVideo = () => {
                     strokeLinejoin="round"
                   />
                 </svg> */}
-                <svg
+                {/* <svg
                   width={64}
                   height={64}
                   viewBox="0 -186 1396 1396"
@@ -230,6 +235,32 @@ export const PopularVideo = () => {
                       d="M375.467499 131.039743h11.809431v113.937289h-11.809431zM760.297654 131.039743h11.809431v113.937289h-11.809431zM488.006632 66.437412h19.449359v165.332033h-19.449359zM640.817672 0h15.978934v206.465305h-15.978934z"
                       fill="#1a9e48"
                     />
+                  </g>
+                </svg> */}
+                <svg
+                  width={64}
+                  height={64}
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g id="SVGRepo_bgCarrier" stroke-width="0" />
+
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+
+                  <g id="SVGRepo_iconCarrier">
+                    {' '}
+                    <path
+                      d="M16 10L18.5768 8.45392C19.3699 7.97803 19.7665 7.74009 20.0928 7.77051C20.3773 7.79703 20.6369 7.944 20.806 8.17433C21 8.43848 21 8.90095 21 9.8259V14.1741C21 15.099 21 15.5615 20.806 15.8257C20.6369 16.056 20.3773 16.203 20.0928 16.2295C19.7665 16.2599 19.3699 16.022 18.5768 15.5461L16 14M6.2 18H12.8C13.9201 18 14.4802 18 14.908 17.782C15.2843 17.5903 15.5903 17.2843 15.782 16.908C16 16.4802 16 15.9201 16 14.8V9.2C16 8.0799 16 7.51984 15.782 7.09202C15.5903 6.71569 15.2843 6.40973 14.908 6.21799C14.4802 6 13.9201 6 12.8 6H6.2C5.0799 6 4.51984 6 4.09202 6.21799C3.71569 6.40973 3.40973 6.71569 3.21799 7.09202C3 7.51984 3 8.07989 3 9.2V14.8C3 15.9201 3 16.4802 3.21799 16.908C3.40973 17.2843 3.71569 17.5903 4.09202 17.782C4.51984 18 5.07989 18 6.2 18Z"
+                      stroke="#55b054"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />{' '}
                   </g>
                 </svg>
               </div>
