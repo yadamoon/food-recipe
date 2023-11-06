d51de52315ae43e7e18bd12f77cdcd2b85ebf9a4
Yared <yared241993@gmail.com>
2023-11-06 13:04:42 +0300

    add before call to check add sweetalert

--- src/components/Navs/Contact.jsx
+++ src/components/Navs/Contact.jsx
@@ -2,13 +2,31 @@ import React from 'react'
 import ContactForm from '../forms/ContactForm'
 
 const Contact = () => {
+  const call = () => {
+    Swal.fire({
+      title: '<strong>Do You Want Call </strong>',
+      icon: 'info',
+      showCloseButton: true,
+      showCancelButton: true,
+      focusConfirm: false,
+      confirmButtonText: `<a className="bg-red-600">cancel</a>`,
+      confirmButtonAriaLabel: 'Cancel',
+      cancelButtonText: `
+        <i class="fa fa-phone"></i>
+      `,
+      cancelButtonAriaLabel: 'Thumbs down',
+    })
+  }
   return (
     <div className="bg-white  mx-3 rounded">
       <br />
       <h1 className="text-center text-2xl font-bold ">CONTACT US</h1>
 
       <div className=" grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-8 sm:grid-cols-2 p-6 ">
-        <div className=" rounded text-center w-full h-auto col-span-1 p-6 border  shadow hover:bg-gray-50">
+        <div
+          className=" rounded text-center w-full h-auto col-span-1 p-6 border  shadow hover:bg-gray-50"
+          onClick={call}
+        >
           <span className="text-2xl text-teal-800">
             <i class="fa fa-phone" aria-hidden="true"></i>
           </span>
