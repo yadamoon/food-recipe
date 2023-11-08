145e6955dd8bf355430fc02cb52b768d8f2a0e36
Yared <yared241993@gmail.com>
2023-11-08 15:50:32 +0300

    updated  button size

--- src/components/Navs/NotFound.jsx
+++ src/components/Navs/NotFound.jsx
@@ -7,7 +7,7 @@ const NotFound = () => {
       <br />
       <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
         <div class="text-center">
-          <p class="text-base font-semibold text-indigo-600">404</p>
+          <p class="text-base font-semibold text-teal-600">404</p>
           <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
             Page not found
           </h1>
@@ -17,12 +17,12 @@ const NotFound = () => {
           <div class="mt-10 flex items-center justify-center gap-x-6">
             <Link
               to="/"
-              class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              class="rounded-md bg-teal-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Go back home
             </Link>
-            <a href="#" class="text-sm font-semibold text-gray-900">
-              Contact support <Link to="/Contact">&rarr;</Link>
+            <a href="#" class="text-sm font-semibold text-teal-900">
+              Contact support <Link to="/contact">&rarr;</Link>
             </a>
           </div>
         </div>
