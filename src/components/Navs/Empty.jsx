5618da555b268257cdb5ba0873deb39bf0eb4f20
Yared <yared241993@gmail.com>
2023-11-15 15:51:57 +0300

    created one functional components

---
+++ src/components/Navs/Empty.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Empty = () => {
+  return (
+    <div className=" flex justify-center bg-white border rounded">
+      <div className="w-full h-full">
+        <p>
+          <span className="text-blu-300 text-3xl">Empty.... </span>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Empty
