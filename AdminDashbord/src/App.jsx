055465fb85251593974570fee170f7a3300ef0f3
Yared <yared241993@gmail.com>
2023-11-16 13:25:14 +0300

    sth updated

--- AdminDashbord/src/App.jsx
+++ AdminDashbord/src/App.jsx
@@ -3,14 +3,11 @@ import SideBar from './components/SideBar/SideBar'
 
 function App() {
   return (
-    <div className="">
+    <div className="flex">
       <div className="basis-[12%] h-[100vh]">
         <SideBar />
       </div>
-      <div className="basis-[88%] border">
-        {' '}
-        <p className="text-black">hello</p>
-      </div>
+      <div className="basis-[88%] h-[100vh]">hello</div>
     </div>
   )
 }
