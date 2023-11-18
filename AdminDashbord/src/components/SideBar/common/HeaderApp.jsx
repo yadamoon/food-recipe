235cf58450526fba78e303f3bb786f131f764f08
Yared <yared241993@gmail.com>
2023-11-18 17:34:30 +0300

    add button to logout

--- AdminDashbord/src/components/SideBar/common/HeaderApp.jsx
+++ AdminDashbord/src/components/SideBar/common/HeaderApp.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Chart from '../../Layout/Chart'
+import { Link } from 'react-router-dom'
 
 export default function HeaderApp() {
   return (
@@ -22,23 +23,10 @@ export default function HeaderApp() {
 
             <div className=" flex space-x-5 justify-center items-center pl-2">
               <div className="relative cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 ">
-                {/* <svg
-                  width={24}
-                  height={24}
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M21 15C21 15.5304 20.7893 16.0391 20.4142 16.4142C20.0391 16.7893 19.5304 17 19 17H7L3 21V5C3 4.46957 3.21071 3.96086 3.58579 3.58579C3.96086 3.21071 4.46957 3 5 3H19C19.5304 3 20.0391 3.21071 20.4142 3.58579C20.7893 3.96086 21 4.46957 21 5V15Z"
-                    stroke="#1F2937"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-                <div className="animate-ping w-1.5 h-1.5 bg-indigo-700 rounded-full absolute -top-1 -right-1 m-auto duration-200" />
-                <div className=" w-1.5 h-1.5 bg-indigo-700 rounded-full absolute -top-1 -right-1 m-auto shadow-lg" /> */}
+                <Link className=" text-white bg-red-600  rounded-lg p-2">
+                  {' '}
+                  Logout{' '}
+                </Link>
               </div>
 
               <img
