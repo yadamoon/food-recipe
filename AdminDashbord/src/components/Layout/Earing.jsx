5363dbe1240d8aad5e43ff9ce54a081d3dcf5be2
Yared <yared241993@gmail.com>
2023-11-22 12:35:11 +0300

    created earing gards

---
+++ AdminDashbord/src/components/Layout/Earing.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+const Earing = () => {
+  return (
+    <div className="">
+      <div className="grid grid-cols-3 gap-4 mb-4 w-full ">
+        <div className="w-full  bg-gray-50 dark:bg-gray-800">
+          <Earing />
+        </div>
+        <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+          <p className="text-2xl text-gray-400 dark:text-gray-500">
+            <svg
+              className="w-3.5 h-3.5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 18 18"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M9 1v16M1 9h16"
+              />
+            </svg>
+            world
+          </p>
+        </div>
+        <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+          <p className="text-2xl text-gray-400 dark:text-gray-500">
+            <svg
+              className="w-3.5 h-3.5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 18 18"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M9 1v16M1 9h16"
+              />
+            </svg>
+            from
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Earing
