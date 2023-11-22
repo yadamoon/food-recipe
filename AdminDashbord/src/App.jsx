cbefb3b934bdeaac65a1df2ee45bf697f1ecc806
Yared <yared241993@gmail.com>
2023-11-22 05:15:18 +0300

    updated sth

--- AdminDashbord/src/App.jsx
+++ AdminDashbord/src/App.jsx
@@ -256,8 +256,9 @@ function App() {
       </aside>
 
       <div className="p-4 sm:ml-64">
-        <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
-          <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
+        <div className="   rounded-lg dark:border-gray-700">
+          <div className=" mb-4 rounded bg-gray-50 dark:bg-gray-800">
+            <p className="text-2xl text-gray-400 dark:text-gray-500"></p>
             <HeaderApp />
           </div>
 
