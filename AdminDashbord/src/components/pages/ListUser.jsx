2ed7ae1be71aa5e0d3c0c4765641a4960884ddb5
Yared <yared241993@gmail.com>
2023-11-23 16:07:10 +0300

    created new functional components to display list of user

---
+++ AdminDashbord/src/components/pages/ListUser.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Table from '../Layout/Table'
+
+const ListUser = () => {
+  return (
+    <div>
+      {/* <Table /> */}
+      hello
+    </div>
+  )
+}
+
+export default ListUser
