6020ad2146343242a5748b5f0d96f5645e1157dd
Yared <yared241993@gmail.com>
2023-11-23 13:04:52 +0300

    add some text

--- AdminDashbord/src/components/Layout/Earing.jsx
+++ AdminDashbord/src/components/Layout/Earing.jsx
@@ -3,49 +3,35 @@ import React from 'react'
 const Earing = () => {
   return (
     <div className="">
-      <div className="grid grid-cols-3 gap-4 mb-4 w-full ">
-        <div className="w-full  bg-gray-50 dark:bg-gray-800">
-          <Earing />
-        </div>
-        <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-          <p className="text-2xl text-gray-400 dark:text-gray-500">
-            <svg
-              className="w-3.5 h-3.5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 18 18"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M9 1v16M1 9h16"
-              />
-            </svg>
-            world
+      <div class="flex flex-col bg-white border border-t-4 border-t-blue-600 shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700 dark:border-t-blue-500 dark:shadow-slate-700/[.7]">
+        <div class="p-4 md:p-5">
+          <h3 class="text-lg font-bold text-gray-800 dark:text-white">
+            Card title
+          </h3>
+          <p class="mt-2 text-gray-500 dark:text-gray-400">
+            With supporting text below as a natural lead-in to additional
+            content.
           </p>
-        </div>
-        <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-          <p className="text-2xl text-gray-400 dark:text-gray-500">
+          <a
+            class="mt-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+            href="#"
+          >
+            Card link
             <svg
-              className="w-3.5 h-3.5"
-              aria-hidden="true"
+              class="flex-shrink-0 w-4 h-4"
               xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
               fill="none"
-              viewBox="0 0 18 18"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
             >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M9 1v16M1 9h16"
-              />
+              <path d="m9 18 6-6-6-6" />
             </svg>
-            from
-          </p>
+          </a>
         </div>
       </div>
     </div>
