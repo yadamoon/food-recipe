5ea05d4bf27cc9d40da1a2d5c81cef5c8da95808
Yared <yared241993@gmail.com>
2023-11-24 05:10:55 +0300

    add Pagination

--- AdminDashbord/src/components/pages/ListUser.jsx
+++ AdminDashbord/src/components/pages/ListUser.jsx
@@ -3,15 +3,40 @@ import Table from '../Layout/Table'
 
 const ListUser = () => {
   return (
-    <div className="w-full h-screen justify-center items-center">
+    <div
+      className="w-full h-screen
+     justify-center items-center"
+    >
       <div className="flex  p-2 mb-4 rounded bg-gray-100">
         <p className="text-2xl text-gray-400 dark:text-gray-500">
           List Of Users
         </p>
       </div>
       <Table />
+      <div className="mt-10">
+        <PaginationComponent />
+      </div>
     </div>
   )
 }
 
 export default ListUser
+
+import { Pagination } from 'flowbite-react'
+import { useState } from 'react'
+
+export function PaginationComponent() {
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const onPageChange = (page) => setCurrentPage(page)
+
+  return (
+    <div className="flex overflow-x-auto sm:justify-center">
+      <Pagination
+        currentPage={currentPage}
+        totalPages={100}
+        onPageChange={onPageChange}
+      />
+    </div>
+  )
+}
