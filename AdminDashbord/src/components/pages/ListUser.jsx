bc66426af41d751e132c22bebced48f1896fbe4c
Yared <yared241993@gmail.com>
2023-11-24 04:30:41 +0300

    updated sth

--- AdminDashbord/src/components/pages/ListUser.jsx
+++ AdminDashbord/src/components/pages/ListUser.jsx
@@ -3,9 +3,13 @@ import Table from '../Layout/Table'
 
 const ListUser = () => {
   return (
-    <div>
-      {/* <Table /> */}
-      hello
+    <div className="w-full h-screen justify-center items-center">
+      <div className="flex  p-2 mb-4 rounded bg-gray-100">
+        <p className="text-2xl text-gray-400 dark:text-gray-500">
+          List Of Users
+        </p>
+      </div>
+      <Table />
     </div>
   )
 }
