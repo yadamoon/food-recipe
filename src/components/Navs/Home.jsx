f1e34463b85d9c08d9555422aed41c88be9bbb97
Yared <yared241993@gmail.com>
2023-12-21 16:50:07 +0300

    updated the landing page

--- src/components/Navs/Home.jsx
+++ src/components/Navs/Home.jsx
@@ -55,14 +55,14 @@ export const Sub = () => {
 // helathy food
 export const FoodForHealthy = () => {
   return (
-    <div div className="flex  justify-center mx-40">
-      <div className="w-full grid grid-cols-2 gap-2">
-        <div className="xl:w-1/2 md:mb-14 xl:mb-0 relative flex items-center justify-center lg:col-span-1 sm:col-span-2 md:col-span-2">
+    <div div className=" mx-5">
+      {/* <div className="w-full grid grid-cols-2 gap-2">
+        <div className=" sm:col-span-2 md:col-span-2 ">
           <motion.img
             src="/images/test.png"
             alt="Envelope with a newsletter"
             role="img"
-            className="h-2/3 lg:w-full w-full bg-white rounded-2xl md:w-1/2"
+            className="h-2/3 lg:w-2/3 bg-white rounded-2xl md:w-1/2 justify-center items-center"
             animate={{ x: 20 }}
           />
         </div>
@@ -92,7 +92,47 @@ export const FoodForHealthy = () => {
             </Link>
           </div>
         </motion.div>
-      </div>
+      </div> */}
+      <section className="bg-white ">
+        <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+          <div className="mr-auto place-self-center lg:col-span-7">
+            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl text-teal-700 ">
+              Eat Healthy Live Healthy
+            </h1>
+            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl ">
+              From checkout to global sales tax compliance, companies around the
+              world use Flowbite to simplify their payment stack.
+            </p>
+            <a
+              href="#"
+              class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-teal-700 hover:bg-teal-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+            >
+              Get started
+              <svg
+                className="w-5 h-5 ml-2 -mr-1"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </a>
+            {/* <a
+              href="#"
+              className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+            >
+              Speak to Sales
+            </a> */}
+          </div>
+          <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+            <img src="/images/test.png" alt="food recipe" />
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
