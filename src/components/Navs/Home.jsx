2533cf6b83c946a060315091c62fcf2404c78610
Yared <yared241993@gmail.com>
2023-12-26 17:03:30 +0300

    sth Updated

--- src/components/Navs/Home.jsx
+++ src/components/Navs/Home.jsx
@@ -57,8 +57,15 @@ export const FoodForHealthy = () => {
   return (
     <div div className=" mx-5">
       <section className="bg-white ">
+        <div className="text-center items-center  text-teal-700 text-3xl space-y-6 font-bold">
+         
+          Welcome to Our Website!
+        </div>
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-          <div className="mr-auto place-self-center lg:col-span-7">
+          <div className=" lg:mt-0 lg:col-span-5 lg:flex">
+            <img src="/images/test.png" alt="food recipe" />
+          </div>
+          <div className="mr-auto place-self-center lg:col-span-7 mx-10">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl text-teal-700 ">
               Welcome to Our Website!
             </h1>
@@ -83,9 +90,6 @@ export const FoodForHealthy = () => {
                 ></path>
               </svg>
             </Link>
-          </div>
-          <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
-            <img src="/images/test.png" alt="food recipe" />
           </div>
         </div>
       </section>
